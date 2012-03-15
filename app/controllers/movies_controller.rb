2e66393adf721980618aac1b4ee8420e466c0958
Keijo Kuusmik <saasbook@saasbook.(none)>
2012-03-15 02:43:44 -0700

    implementing session in progress

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,30 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
+  
+  def storeSession
+    session["ratings"] = @all_ratings
+    session["sort"] = params["sort"]
+  end
+  
+  def tryRestoreSession
+    if (not params["ratings"]) or (not params["sort"])
+      redir_params = Hash.new
+      if not params["ratings"]
+        redir_params = redir_params.merge!({"ratings" => session["ratings"]})
+      else
+        redir_params = redir_params.merge!({"ratings" => params["ratings"]})
+      end
+      
+      if not params["sort"]
+        redir_params = redir_params.merge!({"sort" => session["sort"]})
+      else
+        redir_params = redir_params.merge!({"sort" => params["sort"]})
+      end
+      
+      redirect_to movies_path(redir_params)
+    end
+  end
 
   def getCurrentRatings
     ratings = Hash.new
@@ -18,20 +42,38 @@ class MoviesController < ApplicationController
         end
       }
     else
-      Movie.ratings.each {|rating| 
-        ratings[rating] = true
+      Movie.ratings.each {|rating|
+        ratings[rating] = false
       }
     end
     
     return ratings
   end
+  
+  def getAllRatings
+    ratings = Hash.new
+    
+    Movie.ratings.each {|rating|
+      ratings[rating] = true
+    }
+    
+    return ratings
+  end
 
   def index
+    # if user was refreshing then lets redirect to RESTful page
+    # if user wasn't refreshing and there are no "ratings" and "sort" then user came probably from "/movies"
+    # so lets redirect to RESTful page
+    if params["commit"] == "Refresh"
+      return redirect_to movies_path("ratings" => getCurrentRatings())
+    elsif not params["ratings"] and not params["sort"]
+      return redirect_to movies_path("ratings" => getAllRatings(), "sort" => "title")
+    end
+    
+    tryRestoreSession()
+    
     @movies = Array.new
     @all_ratings = getCurrentRatings()
-    
-    #if params["commit"] == "Refresh"
-    #end
     
     condition = {:conditions => ["rating IN (?)", @all_ratings.select {|k, v| v == true}.keys]}
     
@@ -46,6 +88,8 @@ class MoviesController < ApplicationController
       @release_date_class = ''
       @title_class = ''
     end
+    
+    storeSession()
   end
 
   def new
